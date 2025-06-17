b490f9956dd86a3a92321a22a7ccd57011ecf099
오동연 <dyoh@selper.co.kr>
2025-06-17 13:49:49 +0900

    Add array methods and Date object examples
    Implemented examples for array methods like filter, map, sort, toSorted, and join in chapter09.js. Added date object creation, manipulation, and formatting examples in chapter10.js. Updated index.html to include chapter10.js.

--- section02/chapter09.js
+++ section02/chapter09.js
@@ -1,3 +1,70 @@
 console.log("chapter09");
 
-//
+// array manipulation
+// 1. filter
+// return new array which is fulfilled conditions
+let arr1 = [
+    {name: "test1", hobby: "tennis"},
+    {name: "test2", hobby: "tennis"},
+    {name: "test3", hobby: "read a book"},
+]
+const tennisPeople = arr1.filter((item) => {return item.hobby === "tennis";});
+console.log(tennisPeople);
+const tennisPeople1 = arr1.filter((item) => item.hobby === "tennis");
+console.log(tennisPeople1);
+
+// 2. map
+// iterate all element of array, execute each callback function and return new array which result of callback functions collection
+let arr2 = [1, 2, 3, 4, 5];
+const mapArr1 = arr2.map((value, index, array) => {
+    return value * 2;
+});
+console.log(mapArr1)
+
+const mapArr2 = arr1.map(item => item.name);
+console.log(mapArr2)
+
+// 3. sort
+// sort array element lexicographical asc
+let arr3 = ["c", "a", "b"];
+arr3.sort();
+console.log(arr3);
+
+let arr4 = [16, 2, 1, 15, 3, 14]; // sort() is not working.
+// because it cast to string and sort. sort() check first letter of value.
+// 16's first letter 1 is lower than 2. sort() 16 and 2 return 16, 2
+// let arr4 = [10, 5, 3];
+arr4.sort();
+console.log(arr4);
+
+arr4.sort((a, b) => {
+    if (a > b) {
+        // change position a, b to b, a
+        return 1;
+    } else if (a < b) {
+        // change position b, a to a, b
+        return -1;
+    } else {
+        // do nothing
+        return 0;
+    }
+})
+// arr4.sort((a, b) => a - b);
+console.log(arr4);
+
+// 4. toSorted
+// it functionally same as sort() but sort() is sorting it self. but toSorted() is return new array.
+let arr5 = ["c", "b", "a"];
+const sorted = arr5.toSorted()
+console.log(sorted)
+
+// 5. join
+// concat all element to the one string and return
+let arr6 = ["c", "a", "b"];
+const joined = arr6.join();
+console.log(joined);
+const joined1 = arr6.join("-");
+console.log(joined1);
+
+const joined2 = arr1.join();
+console.log(joined2);
--- section02/chapter10.js
+++ section02/chapter10.js
@@ -1,3 +1,48 @@
 console.log("chapter10");
 
-//
+// date object
+// 1. how to create Date object
+let date1 = new Date();
+console.log(date1);
+
+let date2 = new Date("2020-01-01");
+console.log(date2);
+
+let date3 = new Date("2020-01-01 10:10:10");
+console.log(date3);
+
+let date4 = new Date(2020, 1, 2, 3, 4, 5, 6);
+console.log(date4);
+
+// 2. timestamp
+// get ms number of from UTC("1970.01.01 00:00:00") time to other time
+let ts1 = date1.getTime();
+console.log(ts1);
+
+let date5 = new Date(ts1);
+console.log(date5);
+
+// 3. get time element from Date object
+let year = date1.getFullYear();
+let month = date1.getMonth() + 1;
+let date = date1.getDate();
+let hour = date1.getHours();
+let minute = date1.getMinutes();
+let second = date1.getSeconds();
+let ms = date2.getMilliseconds();
+
+console.log(year, month, date, hour, minute, second, ms);
+
+// 4. manipulate time
+date1.setFullYear(2020);
+date1.setMonth(11);
+date1.setDate(31);
+date1.setHours(23);
+date1.setMinutes(59);
+date1.setSeconds(59);
+date1.setMilliseconds(999);
+console.log(date1)
+
+// 5. get various time format
+console.log(date1.toDateString());
+console.log(date1.toLocaleDateString());
